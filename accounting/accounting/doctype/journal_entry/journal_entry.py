b5741abd50d63c6b5809ceb450335aa3d081f1b7
18alantom <2.alan.tom@gmail.com>
2021-03-31 01:23:08 +0530

    Fix and update GL Entry

--- accounting/accounting/doctype/journal_entry/journal_entry.py
+++ accounting/accounting/doctype/journal_entry/journal_entry.py
@@ -6,24 +6,33 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class JournalEntry(Document):
     def validate(self):
         self.validate_journal_items()
 
     def on_submit(self):
-        self.add_ledger_entries() # General Ledger Entry
+        self.add_ledger_entries()  # General Ledger Entry
+
+    def validate_journal_items(self):
+        self.validate_accounting_item_count()
+        self.validate_credit_debit_values()
+        self.validate_credit_debit_difference()
 
     def validate_accounting_item_count(self):
         item_count = len(self.journal_items)
-        if  item_count != 2:
-            frappe.throw("There should be 2 accounting entries, \n"
-                    "This is double entry accounting, "
-                    f"not {item_count} entry accounting.")
+        if item_count != 2:
+            frappe.throw(
+                "There should be 2 accounting entries, \n"
+                "This is double entry accounting, "
+                f"not {item_count} entry accounting."
+            )
 
     def validate_credit_debit_values(self):
         for item in self.journal_items:
-            is_valid = ( item.credit_in_account == 0 and item.debit_in_account > 0 ) or \
-                       ( item.credit_in_account > 0 and item.debit_in_account == 0 )
+            is_valid = (item.credit_in_account == 0 and item.debit_in_account > 0) or (
+                item.credit_in_account > 0 and item.debit_in_account == 0
+            )
             if not is_valid:
                 frappe.throw("Either credit or debit of an entry should be 0.")
 
@@ -38,18 +47,39 @@ class JournalEntry(Document):
         if abs(diff) > 0:
             frappe.throw("Credit, Debit difference should be 0.")
 
-    def validate_journal_items(self):
-        self.validate_accounting_item_count()
-        self.validate_credit_debit_values()
-        self.validate_credit_debit_difference()
+    def add_ledger_entries(self):
+        # Journal Entries
+        credit = self.get_credit_item()
+        debit = self.get_debit_item()
+
+        # Ledger Entries
+        credit_item = self.get_ledger_item(
+            credit.account,
+            debit.account,
+            credit.credit_in_account,
+            credit.debit_in_account,
+        )
+        debit_item = self.get_ledger_item(
+            debit.account,
+            credit.account,
+            debit.credit_in_account,
+            debit.debit_in_account,
+        )
+
+        # Save Entries
+        credit_item.insert(ignore_permissions=True)
+        debit_item.insert(ignore_permissions=True)
+
+    def get_credit_item(self):
+        return self.get_credit_or_debit_item("credit_in_account")
+
+    def get_debit_item(self):
+        return self.get_credit_or_debit_item("debit_in_account")
 
     def get_credit_or_debit_item(self, account_type):
         for item in self.journal_items:
             if getattr(item, account_type) > 0:
                 return item
-
-    def get_credit_item(self): return self.get_credit_or_debit_item("credit_in_account")
-    def get_debit_item(self): return self.get_credit_or_debit_item("debit_in_account")
 
     def get_ledger_item(self, account, against_account, credit, debit):
         return frappe.get_doc(
@@ -59,20 +89,7 @@ class JournalEntry(Document):
             against_account=against_account,
             credit=credit,
             debit=debit,
-            voucher_type="Journal Entry"
+            voucher_type="Journal Entry",
+            company_name=self.company,
+            voucher_number=self.name,
         )
-
-    def add_ledger_entries(self):
-        # Journal Entries
-        credit = self.get_credit_item()
-        debit = self.get_debit_item()
-
-        # Ledger Entries
-        credit_item = self.get_ledger_item(credit.account, debit.account, \
-                credit.credit_in_account, debit.debit_in_account)
-        debit_item = self.get_ledger_item(debit.account, credit.account, \
-                debit.debit_in_account, credit.credit_in_account)
-
-        # Save Entries
-        credit_item.insert(ignore_permissions=True)
-        debit_item.insert(ignore_permissions=True)
