d2f2e82dffc8fc0c62f907694f193da263158c18
18alantom <2.alan.tom@gmail.com>
2021-03-31 14:49:42 +0530

    Add Total row, and filters

--- accounting/accounting/report/general_ledger/general_ledger.js
+++ accounting/accounting/report/general_ledger/general_ledger.js
@@ -3,7 +3,33 @@
 /* eslint-disable */
 
 frappe.query_reports["General Ledger"] = {
-	"filters": [
-
-	]
+  filters: [
+    {
+      fieldname: "company",
+      label: __("Company"),
+      fieldtype: "Link",
+      options: "Company",
+      mandatory: true,
+    },
+    {
+      fieldname: "from_date",
+      label: __("From Date"),
+      fieldtype: "Date",
+      mandatory: true,
+      default: frappe.datetime.month_start(),
+    },
+    {
+      fieldname: "to_date",
+      label: __("To Date"),
+      fieldtype: "Date",
+      mandatory: true,
+      default: frappe.datetime.now_date(),
+    },
+  ],
 };
+
+frappe.db.get_list("Company").then((c) => {
+  if (c.length > 0) {
+    frappe.query_reports["General Ledger"].filters[0]["default"] = c[0]["name"];
+  }
+});
--- accounting/accounting/report/general_ledger/general_ledger.py
+++ accounting/accounting/report/general_ledger/general_ledger.py
@@ -4,29 +4,55 @@
 from __future__ import unicode_literals
 
 import frappe
+from frappe import _
+from frappe.utils import getdate
+
+blank_line = ["", "", 0, 0, 0, "", "", ""]
+fields = [
+    "posting_date",
+    "account",
+    "debit",
+    "credit",
+    "voucher_type",
+    "voucher_number",
+    "against_account",
+]
 
 
 def execute(filters=None):
-    company_name = "Lin's Landmines"
-    query_filters = dict(company_name=company_name)
+    validate_filters(filters)
+    records = get_db_records(filters)
+    columns = get_report_columns(filters)
+    data = get_report_data(records)
+    return columns, data
 
-    fields = [
-        "posting_date",
-        "account",
-        "debit",
-        "credit",
-        "voucher_type",
-        "voucher_number",
-        "against_account",
-    ]
 
-    currency = frappe.db.get_value("Company", company_name, "currency")
-    records = frappe.db.get_all(
-        "GL Entry", filters=query_filters, fields=fields, order_by="posting_date asc"
-    )
-    #  frappe.throw(str(records))
+def validate_filters(filters):
+    validate_dates(filters["from_date"], filters["from_date"])
 
-    columns = [
+
+def validate_dates(from_date, to_date):
+    from_date = getdate(from_date)
+    to_date = getdate(to_date)
+    if from_date > to_date:
+        frappe.throw(_("From Date can't be after To Date"))
+
+
+def get_db_records(filters):
+    query = f"""
+        select {','.join(fields)}
+        from `tabGL Entry`
+        where
+            posting_date between "{filters["from_date"]}" and "{filters["to_date"]}"
+            and
+            company_name="{filters["company"]}"
+    """
+    return frappe.db.sql(query)
+
+
+def get_report_columns(filters):
+    currency = frappe.db.get_value("Company", filters["company"], "currency")
+    return [
         "Posting Date",
         "Account",
         f"Debit ({currency})",
@@ -36,7 +62,39 @@ def execute(filters=None):
         "Voucher No",
         "Against Account",
     ]
-    data = [[r[f] for f in fields] for r in records]
-    for row in data:
-        row.insert(4, row[2] - row[3])
-    return columns, data
+
+
+def get_report_data(records):
+    # Initialize variables
+    data = []
+    debit_total = credit_total = balance = debit = credit = 0
+    voucher_number = records[0][-2]
+
+    for pd, a, d, c, vt, vn, aa in records:
+        if vn != voucher_number:
+            append_last_line(data, debit, credit)
+            voucher_number = vn
+            credit_total += credit
+            debit_total += debit
+            debit = credit = 0
+
+        credit += c
+        debit += d
+        balance += d - c
+
+        data.append([pd, a, d, c, balance, vt, vn, aa])
+
+    credit_total += credit
+    debit_total += debit
+    append_last_line(data, debit, credit)
+
+    # Footer Total
+    data.append(
+        ["", "Total", debit_total, credit_total, debit_total - credit_total, "", "", ""]
+    )
+    return data
+
+
+def append_last_line(data, debit, credit):
+    data.append(["", "Total", debit, credit, debit - credit, "", "", ""])
+    data.append(blank_line)
